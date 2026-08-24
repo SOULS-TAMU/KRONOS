30ea49563648d3f12b08f9dcd909d1a9f94360ce
toufik3078 <toufikrupom@gmail.com>
2026-08-24 11:49:00 -0500

    Let the built-in problems example finish without matplotlib

--- examples/02_builtin_problems.py
+++ examples/02_builtin_problems.py
@@ -1,6 +1,5 @@
-"""Run several built-in problems and plot one of them."""
+"""Run several built-in problems, and plot one if matplotlib is available."""
 from kronos import find, load_problem, solve
-from kronos.plotting import plot_runs
 
 print(f"{'problem':<14}{'n':>4}{'m':>4}{'conv':>8}{'certified':>11}{'reached f*':>12}")
 for name in find(max_n=12, constrained=True)[:8]:
@@ -11,6 +10,11 @@ for name in find(max_n=12, constrained=True)[:8]:
 
 p = load_problem("hs030")
 r = solve(p, multi_start=True, ms_num_starts=25, ms_seed=42)
-fig = plot_runs(r, fstar=p.fstar)
-fig.savefig("hs030_runs.png", dpi=150)
-print("\nwrote hs030_runs.png")
+try:
+    from kronos.plotting import plot_runs
+    fig = plot_runs(r, fstar=p.fstar)
+except ImportError:
+    print("\nno plot written; for figures install:  pip install kronos[plot]")
+else:
+    fig.savefig("hs030_runs.png", dpi=150)
+    print("\nwrote hs030_runs.png")
